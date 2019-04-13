e082c637165a932d409864511ce58385eda2de45
zhongxia245 <zhongxia245@sina.com>
2019-04-13 22:57:44 +0800

    修复创建页面路径不对问题

--- scripts/generator.js
+++ scripts/generator.js
@@ -2,12 +2,14 @@
  * 根据命令生成页面
  */
 const fs = require('fs')
+const path = require('path')
 const { red } = require('chalk')
 const copyDir = require('../utils/copyDir')
-const { resolveOwn } = require('../config/paths')
+const { resolveOwn, resolveApp } = require('../config/paths')
 
 const generatorPage = async () => {
   let [type, pageName] = process.argv.slice(2)
+  pageName = path.join('src', pageName)
 
   switch (type) {
     case 'page':
@@ -16,7 +18,7 @@ const generatorPage = async () => {
         console.log(red(`[ERROR]: Directory "${pageName}" already exists, please change pageName...`))
         return
       }
-      copyDir(resolveOwn('template/page/simple'), resolveOwn(pageName))
+      copyDir(resolveOwn('template/page/simple'), resolveApp(pageName))
       break
     default:
       // 待实现其他
