92ff3d000c34a02edf1966deb1b7c40f455dba6c
zhongxia245 <zhongxia245@sina.com>
2019-04-21 11:02:02 +0800

    优化 webpack：add happypack , webpack-parallel-uglify-plugin

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,25 +1,31 @@
 /**
  * 2019-04-07 13:29:48
  * webpack 的一些 公共配置
+ * 需要重新组织一下代码
+ * 部分优化，参考：http://louiszhai.github.io/2019/01/04/webpack4/
  */
 const path = require('path')
 const glob = require('glob')
+const os = require('os')
+const HappyPack = require('happypack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 const WebpackBar = require('webpackbar')
 const CONFIG = require('./config')
 const paths = require('./paths')
 
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
+
 /**
  * 根据匹配规则输出指定后缀得文件
- *
  * @param {String}  pattern 匹配规则
  * @param {Boolean} hotReload 是否需要热更新
  */
 const getEntries = (pattern, hotReload) => {
   let fileList = glob.sync(pattern)
   return fileList.reduce((previous, current) => {
-    let filePath = path.parse(path.relative(paths.resolveApp(CONFIG.inputPath), current))
+    let filePath = path.parse(path.relative(paths.resolveApp(''), current))
 
     let withoutSuffix = path.join(filePath.dir, filePath.name)
 
@@ -37,46 +43,34 @@ const getEntries = (pattern, hotReload) => {
   }, {})
 }
 
-// postcss 的配置
-const postCssConfig = {
-  ident: 'postcss',
-  plugins: [
+/**
+ * 获取 postcss 的配置
+ * 可以配置是否使用 rem
+ *
+ * QUESTION: postcssConfig 写在 webpack 的方式，不支持 happyPack
+ * TypeError: Cannot read property 'postcss' of null
+ * 还没有找到解决办法，因此css/less 不使用 happypack
+ */
+const getPostcssConfig = () => {
+  let plugins = [
     require('postcss-import')(),
     require('postcss-preset-env')({
       browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4']
-    })
+    }),
+    require('cssnano')()
   ]
-}
-
-if (CONFIG.usePx2Rem) {
-  postCssConfig.plugins.push(
-    require('postcss-pxtorem')({
-      rootValue: CONFIG.basePixel,
-      propWhiteList: []
-    })
-  )
-}
-
-// babel 配置
-const babelOptions = {
-  plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        absoluteRuntime: false,
-        corejs: false,
-        helpers: true,
-        regenerator: true,
-        useESModules: false
-      }
-    ],
-    require.resolve('@babel/plugin-transform-react-jsx'),
-    require.resolve('@babel/plugin-syntax-dynamic-import'),
-    require.resolve('@babel/plugin-proposal-class-properties'),
-    require.resolve('styled-jsx/babel'),
-    ['import', { libraryName: 'antd-mobile', style: 'css' }]
-  ],
-  presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-react')]
+  if (CONFIG.usePx2Rem) {
+    plugins.push(
+      require('postcss-pxtorem')({
+        rootValue: CONFIG.basePixel,
+        propWhiteList: []
+      })
+    )
+  }
+  return {
+    ident: 'postcss',
+    plugins: plugins
+  }
 }
 
 /**
@@ -124,8 +118,50 @@ module.exports = (isDev, inputPath = '') => {
     htmlPlugins.push(new HtmlWebpackPlugin(config))
   }
 
+  const babelOptions = {
+    cacheDirectory: true,
+    plugins: [
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          absoluteRuntime: false,
+          corejs: false,
+          helpers: true,
+          regenerator: true,
+          useESModules: false
+        }
+      ],
+      require.resolve('@babel/plugin-transform-react-jsx'),
+      require.resolve('@babel/plugin-syntax-dynamic-import'),
+      require.resolve('@babel/plugin-proposal-class-properties'),
+      require.resolve('styled-jsx/babel'),
+      ['import', { libraryName: 'antd-mobile', style: 'css' }]
+    ],
+    presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-react')]
+  }
+
   // 插件配置
   let plugins = [
+    new HappyPack({
+      id: 'pug',
+      threadPool: happyThreadPool,
+      loaders: ['pug-loader']
+    }),
+    new HappyPack({
+      id: 'json',
+      threadPool: happyThreadPool,
+      loaders: ['json-loader']
+    }),
+    new HappyPack({
+      id: 'jsx',
+      threadPool: happyThreadPool,
+      loaders: [
+        {
+          loader: 'babel-loader',
+          options: babelOptions
+        }
+      ]
+    }),
     ...htmlPlugins,
     new MiniCssExtractPlugin({
       filename: CONFIG.isLocal ? '[name].css' : '[name]-[contenthash].css'
@@ -163,6 +199,25 @@ module.exports = (isDev, inputPath = '') => {
       chunkFilename: '[name].js'
     },
     optimization: {
+      minimizer: [
+        new ParallelUglifyPlugin({
+          // 多进程压缩
+          cacheDir: '.cache/',
+          uglifyJS: {
+            output: {
+              comments: false,
+              beautify: false
+            },
+            compress: {
+              warnings: false,
+              drop_console: true,
+              collapse_vars: true,
+              reduce_vars: true
+            }
+          }
+        })
+      ],
+      // 代码分割
       splitChunks: {
         cacheGroups: {
           default: {
@@ -176,27 +231,28 @@ module.exports = (isDev, inputPath = '') => {
       }
     },
     resolve: {
+      /**
+       * 没必要一层一层去找第三方模块
+       * webpack 默认去 node_modules 找第三方模块
+       * 如果没有找到，则往 ../node_modules 继续找
+       * 一直找到根目录，如果没有，则报找不到模块
+       */
+      // modules: [paths.resolveApp('node_modules')],
       extensions: ['.js', '.jsx', 'json'],
       alias: CONFIG.alias
     },
     module: {
+      // 忽略未采用模块化的文件，因此jquery或lodash将不会被下面的loaders解析
       rules: [
         {
           test: /\.pug$/,
-          use: 'pug-loader'
+          use: 'happypack/loader?id=pug',
+          exclude: /node_modules/
         },
-        {
-          test: /\.json$/,
-          use: 'json-loader'
-        },
+
         {
           test: /\.jsx?$/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: babelOptions
-            }
-          ],
+          use: 'happypack/loader?id=jsx',
           exclude: /node_modules/
         },
         {
@@ -206,7 +262,7 @@ module.exports = (isDev, inputPath = '') => {
             'css-loader',
             {
               loader: 'postcss-loader',
-              options: postCssConfig
+              options: getPostcssConfig()
             }
           ]
         },
@@ -217,51 +273,39 @@ module.exports = (isDev, inputPath = '') => {
             'css-loader',
             {
               loader: 'postcss-loader',
-              options: postCssConfig
+              options: getPostcssConfig()
             },
-            {
-              loader: 'less-loader',
-              options: CONFIG.lessOption
-            }
+            'less-loader'
           ]
+        },
+        {
+          test: /\.json$/,
+          use: 'happypack/loader?id=json',
+          exclude: /node_modules/
         },
         {
           test: /\.(jpg|png|gif|svg|ico)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 1024 * 8,
-                outputPath: `${CONFIG.inputPath}/assets/images`,
-                name: '[name]-[hash].[ext]',
-                fallback: 'file-loader'
-              }
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 1024 * 8,
+              outputPath: `${CONFIG.inputPath}/assets/images`,
+              name: '[name]-[hash].[ext]',
+              fallback: 'file-loader'
             }
-          ]
+          },
+          exclude: /node_modules/
         },
         {
-          test: /.(eot|woff|woff2|ttf)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: `${CONFIG.inputPath}/assets/fonts`,
-                name: '[name]-[hash].[ext]'
-              }
+          test: /.(eot|woff|woff2|ttf|doc|docx|ppt|pptx|xls|xlsx|pdf|txt|zip|mp3|mp4)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              outputPath: `${CONFIG.inputPath}/assets/file`,
+              name: '[name]-[hash].[ext]'
             }
-          ]
-        },
-        {
-          test: /.(doc|docx|ppt|pptx|xls|xlsx|pdf|txt|zip|mp3|mp4)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: `${CONFIG.inputPath}/assets/files`,
-                name: '[name]-[hash].[ext]'
-              }
-            }
-          ]
+          },
+          exclude: /node_modules/
         }
       ]
     },
